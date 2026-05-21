65c2c081eb7d18e98c8255b4ad136c49ee8c61db
Kailyn <hsinlee.5in@gmail.com>
2026-05-21 15:50:06 +0800

    add graffiti

---
+++ students/Kailyn/scripts/graffiti.js
@@ -0,0 +1,17 @@
+
+//let picture= document.getElementById("dog-picture");
+//let artFrame = document.getElementById("art-frame");
+//let aart= document.getElementById("art");
+let myGraffiti = document.getElementById("my-graffiti");
+//let button=document.getElementById("clean");
+let surface=myGraffiti.getContext("2d");
+console.log(myGraffiti);
+
+
+//function draw(){}
+
+
+
+//function Clean(){}
+
+//button.addEventListener("click", Clean);

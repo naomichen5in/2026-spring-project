72cb4b9735c6ed868345f18da6b38078313303e6
Rex <rexlee.5in@gmail.com>
2026-05-25 11:28:11 +0800

    Clean button

--- students/Rex/scripts/graffiti.js
+++ students/Rex/scripts/graffiti.js
@@ -1,3 +1,47 @@
-const graffitiCanvas = document.getElementById("my-graffiti")
-console.log(graffitiCanvas)
+/*
+*HTML elements
+*/
+const graffitiCanvas = document.getElementById("my-graffiti");
 const surface = graffitiCanvas.getContext("2d");
+const cleanbutton = document.getElementById("clean");
+
+
+/*
+graffiti style
+*/
+
+surface.lineWidth = 13;
+surface.lineJoin = "round";
+surface.strokeStyle = "red";
+
+/*
+*shape
+*/
+
+function shape(){
+surface.rect(100, 200, 100, 100);
+surface.stroke();
+
+surface.beginPath();
+surface.moveTo(150,50);
+surface.lineTo(100,200);
+surface.closePath();
+surface.stroke();
+
+surface.beginPath();
+surface.moveTo(225,50);
+surface.lineTo(230,150);
+surface.moveTo(220,50);
+surface.lineTo(325,50);
+surface.moveTo(325,50);
+surface.lineTo(310,300);
+surface.closePath();
+surface.stroke();
+
+};
+shape();
+
+function cleanCanvas(){
+    surface.clearRect(0, 0, 400, 400);
+};
+cleanbutton.addEventListener("click", cleanCanvas);

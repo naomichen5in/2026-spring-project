1a1ea204ed73011b81870ddaf31dc7eb58e2a904
Alson_Chuang <alsonchuang.5in@gmail.com>
2026-05-27 15:58:40 +0800

    Draw graffiti

--- students/Alson/scripts/graffiti.js
+++ students/Alson/scripts/graffiti.js
@@ -1,5 +1,65 @@
-const graffiti = document.getElementById("my-graffiti");
+/*
+ * HTML elements
+ */
+const graffitiCanvas = document.getElementById("my-graffiti");
+const surface = graffitiCanvas.getContext("2d");
+const cleanButton = document.getElementById("clean");
 
-console.log(graffitiCanvas);
+/*
+ * Graffiti style.
+ */
+surface.lineWidth = 13;
+surface.lineJoin = "round";
+surface.strokeStyle = "red";
 
-const surface = graffitiCanvas.getElementById("2d");
+/*
+ * Shapes
+ */
+function shapes() {
+    surface.rect(150, 200, 100, 100);
+    surface.stroke();
+
+    surface.beginPath();
+    surface.moveTo(200, 50);
+    surface.lineTo(150, 200);
+    // Add a 7.
+    surface.moveTo(250, 50);
+    surface.lineTo(350, 50);
+    surface.moveTo(350, 50);
+    surface.lineTo(270, 300);
+
+    surface.closePath();
+    surface.stroke();
+}
+shapes();
+
+/*
+ * Clean up
+ */
+function cleanCanvas() {
+    surface.clearRect(0, 0, 400, 400);
+}
+cleanButton.addEventListener("click", cleanCanvas);
+
+/*
+* Graffiti
+*/
+let oldX = 0;
+let oldY = 0;
+function graffiti(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    console.log(x, y, event.buttons);
+
+    if (event.buttons === 1){
+    surface.beginPath();
+    surface.moveTo(oldX, oldY);
+    surface.lineTo(x, y);
+    surface.closePath();
+    surface.stroke();
+    }
+
+    oldX = x;
+    oldY = y;
+}
+graffitiCanvas.addEventListener("mousemove", graffiti);

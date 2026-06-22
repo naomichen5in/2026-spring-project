e3377d10423af5745f98f383bd600f9620e5b519
Naomi <naomichen.5in@gmail.com>
2026-06-22 11:31:55 +0800

    Add duck

---
+++ students/naomi/scripts/dog.js
@@ -0,0 +1,18 @@
+import { ModelRotator } from "../../../scripts/model-rotator.js";
+
+const dog = new ModelRotator('dog-3d');
+const model = "/images/Duck.glb";
+const scale = 1;
+const position = {
+   x:5,
+   y:4,
+   z:-5,
+
+};
+
+//dog.loadCube();
+dog.load(model, scale);
+
+dog.animate();
+dog.cameraPosition(position);
+dog.loadFloor();
